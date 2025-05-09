40933d36285a23e8c1842ff15fec84bc6a1f32bc
AlpernMariana <marianalucia.alpern@estudiantes.unahur.edu.ar>
2025-05-08 23:28:47 -0300

    casi terminado

--- src/main/java/ar/edu/unahur/obj2/Archivo.java
+++ src/main/java/ar/edu/unahur/obj2/Archivo.java
@@ -15,26 +15,59 @@ public class Archivo implements IElemento{
 
     @Override
     public String nombre() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'nombre'");
+        return nombre + "." + extension;
     }
-
     @Override
     public Integer tamanio() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'tamanio'");
+      return tamanio;
     }
 
     @Override
     public void mostrar(Integer identacion) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+       System.out.println("".repeat(identacion) + "-" + this.nombre());
     }
 
     @Override
     public IElemento archivoMasPesado() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'archivoMasPesado'");
+        return this;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        result = prime * result + ((tamanio == null) ? 0 : tamanio.hashCode());
+        result = prime * result + ((extension == null) ? 0 : extension.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Archivo other = (Archivo) obj;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        if (tamanio == null) {
+            if (other.tamanio != null)
+                return false;
+        } else if (!tamanio.equals(other.tamanio))
+            return false;
+        if (extension == null) {
+            if (other.extension != null)
+                return false;
+        } else if (!extension.equals(other.extension))
+            return false;
+        return true;
+    }
+    
 
 }
--- src/main/java/ar/edu/unahur/obj2/Carpeta.java
+++ src/main/java/ar/edu/unahur/obj2/Carpeta.java
@@ -1,9 +1,10 @@
 package ar.edu.unahur.obj2;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-public class Carpeta {
+public class Carpeta implements IElemento{
     private List<IElemento> elementos = new ArrayList<>();
     private String nombre;
 
@@ -13,6 +14,26 @@ public class Carpeta {
     }
     public void agregar(IElemento elemento){
         elementos.add(elemento);
+    }
+    @Override
+    public String nombre() {
+       return nombre;
+    }
+    @Override
+    public Integer tamanio() {
+        return elementos.stream().mapToInt(elemento -> elemento.tamanio()).sum();
+    }
+    @Override
+    public void mostrar(Integer identacion) {
+        System.out.println("".repeat(identacion) + " └" + nombre + " (" + this.tamanio() + "-Bytes) ");
+        elementos.stream().forEach(e -> e.mostrar(identacion + 3));
+    }
+    @Override
+    public IElemento archivoMasPesado() {
+        return elementos.stream().map(elemento -> elemento.archivoMasPesado())
+        .max(
+          Comparator.comparingInt(elemento -> elemento.tamanio())
+        ).orElseThrow();
     }
 
 }
